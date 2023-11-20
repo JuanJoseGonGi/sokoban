dd575a4babb4639ebb59b3b5322ffb5307d3f576
Juan José González Giraldo <juan.1701716389@ucaldas.edu.co>
2023-11-20 09:32:55 -0500

    Refactor UCS algorithm to improve efficiency

--- src/ai/ucs.py
+++ src/ai/ucs.py
@@ -14,22 +14,27 @@ def ucs(model, origin, destination):
 
     came_from = {}
     cost_so_far = {origin: 0}
-    visited = []  # Lista para almacenar los nodos visitados en orden
+    visited = []  # Conjunto de nodos visitados
+    explored = []  # Conjunto de nodos explorados
 
     while open_set:
         current_cost, current = heapq.heappop(open_set)
-        visited.append(current)
 
         if current == destination:
             path = reconstruct_path(came_from, current)
-            return visited, path  # Retorna las posiciones visitadas y la ruta
+            visited.append(current)
+            return list(visited), path  # Retorna las posiciones visitadas y la ruta
 
-        for neighbor in model.get_valid_move_neighbors(current):
-            new_cost = cost_so_far[current] + 1  # asumimos costo = 1 para cada paso
-            if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
-                cost_so_far[neighbor] = new_cost
-                priority = new_cost
-                heapq.heappush(open_set, (priority, neighbor))
-                came_from[neighbor] = current
+        if current not in explored:
+            explored.append(current)
+            visited.append(current)
 
-    return visited, []
+            for neighbor in model.get_valid_move_neighbors(current):
+                new_cost = cost_so_far[current] + 1  # Asumimos costo = 1 para cada paso
+                if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+                    cost_so_far[neighbor] = new_cost
+                    priority = new_cost
+                    heapq.heappush(open_set, (priority, neighbor))
+                    came_from[neighbor] = current
+
+    return list(visited), []
