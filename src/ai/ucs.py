4767e77b79e4f48463e47ddb2c3de6c7cbf0dcf5
Juan José González Giraldo <juan.1701716389@ucaldas.edu.co>
2023-11-20 09:47:27 -0500

    Refactor UCS algorithm to use deque instead of heapq

--- src/ai/ucs.py
+++ src/ai/ucs.py
@@ -1,4 +1,4 @@
-import heapq
+from collections import deque
 
 
 def ucs(model, origin, destination):
@@ -9,16 +9,15 @@ def ucs(model, origin, destination):
             total_path.append(current)
         return total_path[::-1]
 
-    open_set = []
-    heapq.heappush(open_set, (0, origin))
+    queue: deque[tuple[int, tuple[int, int]]] = deque([(0, origin)])
 
     came_from = {}
     cost_so_far = {origin: 0}
     visited = []  # Conjunto de nodos visitados
     explored = []  # Conjunto de nodos explorados
 
-    while open_set:
-        current_cost, current = heapq.heappop(open_set)
+    while queue:
+        current_cost, current = queue.pop()
 
         if current == destination:
             path = reconstruct_path(came_from, current)
@@ -34,7 +33,7 @@ def ucs(model, origin, destination):
                 if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
                     cost_so_far[neighbor] = new_cost
                     priority = new_cost
-                    heapq.heappush(open_set, (priority, neighbor))
+                    queue.appendleft((priority, neighbor))
                     came_from[neighbor] = current
 
     return list(visited), []
